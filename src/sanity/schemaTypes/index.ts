30eaf84085856808ec90b86f67f63301e507c191
Ruslan <ruslan.berendeev@gmail.com>
2025-03-19 13:47:54 +0700

    create sanity schemas

---
+++ src/sanity/schemaTypes/index.ts
@@ -0,0 +1,10 @@
+// ./src/sanity/schemaTypes/index.ts
+import type { SchemaTypeDefinition } from "sanity"
+import { authorType } from "./author"
+import { blockContentType } from "./blockContent"
+import { categoryType } from "./category"
+import { postType } from "./post"
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [authorType, blockContentType, categoryType, postType],
+}
