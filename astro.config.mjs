96335f4ed18c2329d32d9bda1a5760f932799fab
Ruslan <ruslan.berendeev@gmail.com>
2025-03-19 13:43:55 +0700

    initialize sanity project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+
+import sanity from "@sanity/astro"
+import react from "@astrojs/react"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanity({
+      // TODO Issue: use the env file instead [https://docs.astro.build/en/guides/environment-variables/#in-the-astro-config-file]
+      // TODO change the variables to the .env values
+      projectId: "pr5qxvq9",
+      dataset: "production",
+      useCdn: false,
+      apiVersion: "2025-01-28", // TODO insert the current date to access the latest version of the API
+      studioBasePath: "/studio",
+    }),
+    react(),
+  ],
+})
