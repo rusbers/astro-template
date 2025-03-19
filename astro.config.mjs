ec8547aaa9e5d41056152751528edfbd3a789755
Ruslan <ruslan.berendeev@gmail.com>
2025-03-19 20:02:35 +0700

    add visual editing - not working with images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,28 @@
+// astro.config.mjs
 import { defineConfig } from "astro/config"
 
 import sanity from "@sanity/astro"
 import react from "@astrojs/react"
 
+import { loadEnv } from "vite"
+const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  ""
+)
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     sanity({
-      // TODO Issue: use the env file instead [https://docs.astro.build/en/guides/environment-variables/#in-the-astro-config-file]
-      // TODO change the variables to the .env values
-      projectId: "pr5qxvq9",
-      dataset: "production",
-      useCdn: false,
-      apiVersion: "2025-01-28", // TODO insert the current date to access the latest version of the API
+      projectId: PUBLIC_SANITY_PROJECT_ID,
+      dataset: PUBLIC_SANITY_DATASET,
+      useCdn: false, // See note on using the CDN
+      apiVersion: "2025-03-19", // insert the current date to access the latest version of the API
       studioBasePath: "/studio",
+      stega: {
+        studioUrl: "/studio",
+      },
     }),
     react(),
   ],
--- src/sanity/lib/load-query.ts
+++ src/sanity/lib/load-query.ts
@@ -2,6 +2,9 @@
 import { type QueryParams } from "sanity"
 import { sanityClient } from "sanity:client"
 
+const visualEditingEnabled = import.meta.env.PUBLIC_SANITY_VISUAL_EDITING_ENABLED === "true"
+const token = import.meta.env.SANITY_API_READ_TOKEN
+
 export async function loadQuery<QueryResponse>({
   query,
   params,
@@ -9,11 +12,25 @@ export async function loadQuery<QueryResponse>({
   query: string
   params?: QueryParams
 }) {
-  const { result } = await sanityClient.fetch<QueryResponse>(query, params ?? {}, {
+  if (visualEditingEnabled && !token) {
+    throw new Error(
+      "The `SANITY_API_READ_TOKEN` environment variable is required during Visual Editing."
+    )
+  }
+
+  const perspective = visualEditingEnabled ? "previewDrafts" : "published"
+
+  const { result, resultSourceMap } = await sanityClient.fetch<QueryResponse>(query, params ?? {}, {
     filterResponse: false,
+    perspective,
+    resultSourceMap: visualEditingEnabled ? "withKeyArraySelector" : false,
+    stega: visualEditingEnabled,
+    ...(visualEditingEnabled ? { token } : {}),
   })
 
   return {
     data: result,
+    sourceMap: resultSourceMap,
+    perspective,
   }
 }
